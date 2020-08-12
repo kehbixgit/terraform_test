24afded47d22294fe175c1d351fa917cea8c57d4
kehbixgit <kehbix@gmail.com>
2020-08-11 23:56:24 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,3 +14,12 @@ resource "aws_instance" "frontend"{
     Maintainer = "Savatora"
   }
 }
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "demo-vpc"
+    Purpose = "Jenkins-Terraform Demo"
+  }
+}
